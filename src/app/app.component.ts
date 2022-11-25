fa16bdc56e4d811dc66aa2af5627bf5aedc75931
Juan Herrera <david.juanherrera@gmail.com>
2022-11-25 19:16:01 +0100

    add giphy request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'improv';
+  gifUrl;
+  constructor() {
+    this.fetchShirt();
+  }
+
+  async fetchShirt() {
+    const url = new URL('https://api.giphy.com/v1/gifs/search');
+    url.searchParams.set('api_key', '');
+    url.searchParams.set('limit', '1');
+    url.searchParams.set('q', 'shirt');
+    const response = await (await fetch(url.href)).json();
+    this.gifUrl = response.data[0].images.original.url;
+  }
 }
