38f31f2752ac8531279669435f56d4e7c5e81b65
Juan Herrera <david.juanherrera@gmail.com>
2022-11-26 08:08:47 +0100

    add counter api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+export const enum CounterOperation {
+  Fetch = 'get',
+  Hit = 'hit',
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'improv';
+  counter?: number;
+
+  constructor() {
+    this.fetchCounter();
+  }
+
+  async fetchCounter() {
+    this.manageCounter(CounterOperation.Fetch);
+  }
+
+  async increaseCounter() {
+    this.manageCounter(CounterOperation.Hit);
+  }
+
+  async manageCounter(operation: CounterOperation) {
+    const url = `https://api.countapi.xyz/${operation}/improv-burundi/visits`;
+    const response = await (await fetch(url)).json();
+    this.counter = response.value;
+    this.isItFinished();
+  }
+
+  isItFinished() {
+    if (this.counter && this.counter > 14) {
+      new Audio('../assets/trumpet.mp3').play();
+    }
+  }
 }
