cf215cd7d2389d44e4f6dbbc7694d93e4372256f
Juan Herrera <david.juanherrera@gmail.com>
2022-12-13 09:28:19 +0100

    add example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  addDoc,
+  CollectionReference,
+  getCountFromServer,
+  query,
+  orderBy,
+  doc,
+  deleteDoc,
+  PartialWithFieldValue,
+  QueryDocumentSnapshot,
+} from '@angular/fire/firestore';
+
+export interface Message {
+  text: string;
+  createdAt: number;
+  id?: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'improv';
+  messagesRef: CollectionReference<Message>;
+  messageCount: number = 0;
+  messages: Message[] = [];
+  readonly COLLECTION_NAME = 'messages';
+
+  constructor(private db: Firestore) {
+    this.messagesRef = collection(db, this.COLLECTION_NAME).withConverter(this.converter<Message>());
+    collectionData(query(this.messagesRef, orderBy('createdAt')), { idField: 'id' }).subscribe(
+      (data) => (this.messages = data)
+    );
+    this.getMessageCount();
+  }
+
+  converter = <T>() => ({
+    toFirestore: (data: PartialWithFieldValue<T>) => data,
+    fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as T,
+  });
+
+  async addMessage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const createdAt = new Date().getTime();
+    await addDoc(this.messagesRef, { text: input.value, createdAt });
+    await this.getMessageCount();
+    input.value = '';
+  }
+
+  async getMessageCount() {
+    this.messageCount = (await getCountFromServer(query(this.messagesRef))).data().count;
+  }
+
+  async eraseAllMessages() {
+    this.messages.forEach(async ({ id }) => deleteDoc(doc(this.db, this.COLLECTION_NAME, id ?? '')));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
