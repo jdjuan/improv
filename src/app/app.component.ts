1e62f00a60f82e7936fd03f24f2a4d0e8baad3da
Juan Herrera <david.juanherrera@gmail.com>
2023-03-19 22:50:07 +0100

    add fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'improv';
+  openAIKey = '';
+
+  constructor() {
+    this.fetch();
+  }
+
+  async fetch() {
+    console.log('Loading...');
+    const body = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: 'What is the OpenAI mission? Be super concise' }],
+    };
+    const headers = { Authorization: `Bearer ${this.openAIKey}`, 'Content-Type': 'application/json' };
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      headers,
+      body: JSON.stringify(body),
+      method: 'POST',
+    });
+    const data = await response.json();
+    console.log('Done!');
+    console.log(data.choices[0].message.content);
+  }
 }
