9a4503a7c11e1dc49ec4517cdff07a1f0ec76e42
Juan Herrera <david.juanherrera@gmail.com>
2023-03-19 22:33:14 +0100

    rock artist information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'improv';
+  readonly CELEBRITY_ENDPOINT = 'https://api.api-ninjas.com/v1/celebrity?name=';
+  artistData;
+
+  async fetchArtistData(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const artist = input.value;
+
+    const headers = { 'X-Api-Key': 't0jbymptmoGMx9wsd7beU0GdXScfTspHKUtbemIS' };
+    const response = await fetch(`${this.CELEBRITY_ENDPOINT}${artist}`, { headers });
+    this.artistData = await response.json();
+  }
 }
